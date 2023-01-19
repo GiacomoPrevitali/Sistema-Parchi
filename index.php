93a369fbf5783e3ad3c6896e3004c962b09c1932
Giacomo Previtali <giacomoprevitali26@gmail.com>
2023-01-19 18:30:48 +0100

    Programma "Finito"

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -6,27 +10,22 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css" />
+        <script type="text/javascript" src="script.js"></script>
   </head>
 </head>
 <body>
 
-
-
-
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+<center>
+    
         <form method="GET" action="index.php">
-        <select name="Nome_Parco" class="btn btn-outline-success"">
+        <select name="Nome_Parco" id="Nome_P" class="btn btn-outline-success">
+        <option >Seleziona Parco</option>
         <?php
+        
             $ip= '127.0.0.1';
             $username='root';
             $password='';
             $database='sistem';
-
             $connection=new mysqli($ip,$username,$password,$database);
             if ($connection->connect_error) {
                 die('C\'è stato un errore: ' . $connection->connect_error);
@@ -35,28 +34,29 @@
             $result =$connection->query($sql);
             if($result->num_rows>0){
                 while($row=$result->fetch_assoc()){
-                    echo  '<option >' . $row['Nome_Parco'] .'</option>';
+                  echo  '<option >' . $row['Nome_Parco'] .'</option>';   
                 }
+
             }
+            
         ?>
         </select>
+        
             <button class="btn btn-outline-success" type="submit">Cerca</button>
         </form>
-    </div>
-  </div>
-</nav>
-
-
-
+       
 
 
 
 
 
 <form method="GET" action="index.php">
-<select name="Specie_Richiesta" class="btn btn-outline-success"">
+<select name="Specie_Richiesta" id="i_specie" class="btn btn-outline-success"">
+<option >Seleziona Specie</option>
   <?php
-  if(isset($_REQUEST['Nome_Parco'])){
+  if(isset($_GET['Nome_Parco'])){
+    $_SESSION['N_Parco']=$_GET['Nome_Parco'];
+    echo'<script>document.getElementById("Nome_P").value="'.$_SESSION["N_Parco"].'"</script>';
     $ip= '127.0.0.1';
     $username='root';
     $password='';
@@ -66,40 +66,42 @@
     if ($connection->connect_error) {
         die('C\'è stato un errore: ' . $connection->connect_error);
     }
-    $sql='SELECT DISTINCT Id_Specie FROM animale WHERE Id_Parco="'.$_REQUEST['Nome_Parco'].'"';
+    $sql='SELECT DISTINCT Id_Specie FROM animale WHERE Id_Parco="'.$_SESSION['N_Parco'].'"';
     $result =$connection->query($sql);
     
     if($result->num_rows>0){
       while($row=$result->fetch_assoc()){
       
         echo  '<option >'.$row['Id_Specie'].'</option>';
-        //$count=count($s);
+        
       }
     }else{
         echo  '<option >'."Nessun utente trovato".'</option>';
     }
-    
   }
   ?>
 </select>
-<input type="hidden" name="Nome_Parco" value="<?php echo $_REQUEST['Nome_Parco']; ?>">
+<input   type="hidden"name="Nome_Parco" value="<?php echo $_GET['Nome_Parco']; ?>">
 <button class="btn btn-outline-success" type="submit">Cerca</button>
 </form>
-
+ <br>
 
 
 <?php
-if(isset($_REQUEST['Specie_Richiesta'])){
+if(isset($_GET['Specie_Richiesta'])){
   $ip= '127.0.0.1';
   $username='root';
   $password='';
   $database='sistem';
-
+  $_SESSION['specie'] = $_GET['Specie_Richiesta'];
+  echo'<script>document.getElementById("i_specie").value="'.$_SESSION["specie"].'"</script>';
+  echo'<script>document.getElementById("Nome_P").value="'.$_SESSION["N_Parco"].'"</script>';
+  //echo $_GET['Nome_Parco'];
   $connection=new mysqli($ip,$username,$password,$database);
   if ($connection->connect_error) {
       die('C\'è stato un errore: ' . $connection->connect_error);
   }
-  $sql='SELECT Data_Nascita FROM animale WHERE Id_Specie="'.$_REQUEST['Specie_Richiesta'].'" AND Id_Parco="'.$_GET['Nome_Parco'].'"';
+  $sql='SELECT Data_Nascita FROM animale WHERE Id_Specie="'.$_REQUEST['Specie_Richiesta'].'" AND Id_Parco="'.$_SESSION['N_Parco'].'"';
   $result =$connection->query($sql);
   $year=0;
       $month=0;
@@ -113,17 +115,16 @@ if(isset($_REQUEST['Specie_Richiesta'])){
       
       $diff = $data_finale->diff($data_iniziale);
       $year=+$diff->y;
-      $month=+$diff->m;
-      $day=$day+$diff->d;
       $count++;
-      //echo $count;
     }
-    $tot=$year*365+$month*30+$day;
+    $media=$year/$count;
+    $media=floor($media);
     if($count==1){
-      echo "In questo parco è presente: ". $count. " esemplare di ".$_REQUEST['Specie_Richiesta']. " con un'età di ".$tot/$count;
+      echo "In questo parco è presente: ". $count. " esemplare di ".$_REQUEST['Specie_Richiesta']. " con un'età di ".$media. " anni";
     }else{
-    echo "In questo parco sono presenti: ". $count. " esemplari di ".$_REQUEST['Specie_Richiesta']. " con un'età media di ".$tot/$count;
-    }
+        echo "In questo parco sono presenti: ". $count. " esemplari di ".$_REQUEST['Specie_Richiesta']. " con un'età media di ".$media. " anni";
+      }
+    
     
   }else{
       echo  '<option >'."Nessun utente trovato".'</option>';
@@ -133,5 +134,6 @@ if(isset($_REQUEST['Specie_Richiesta'])){
 
 
 ?>
+</center>
 </body>
 </html>
